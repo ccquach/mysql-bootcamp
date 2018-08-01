bbdee8342c5352356401b82832d159b4b96b0b47
cynthia.quach <ccquach93@gmail.com>
2018-08-01 15:14:59 -0700

    build join us app

--- join-us-app/index.js
+++ join-us-app/index.js
@@ -1,5 +1,12 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 var faker = require('faker');
 var mysql = require('mysql');
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
 var connection = mysql.createConnection({
   host: 'localhost',
@@ -9,6 +16,24 @@ var connection = mysql.createConnection({
 });
 
 connection.connect();
+
+app.get('/', function(req, res) {
+  const q = 'SELECT COUNT(*) AS total FROM users';
+  connection.query(q, function(err, results) {
+    if (err) throw err;
+    const total = results[0].total;
+    // res.send(`There are ${total} users in our database`);
+    res.render('home', { total });
+  });
+});
+
+app.post('/register', function(req, res) {
+  var person = { email: req.body.email };
+  connection.query('INSERT INTO users SET ?', person, function(error, results) {
+    if (error) throw error;
+    res.redirect('/');
+  });
+});
 
 // SELECTING DATA ======================================================================
 
@@ -43,16 +68,20 @@ connection.connect();
 
 // BULK INSERT DATA ======================================================================
 
-var data = [];
-for (let i = 0; i < 500; i++) {
-  data.push([faker.internet.email(), faker.date.past()]);
-}
+// var data = [];
+// for (let i = 0; i < 500; i++) {
+//   data.push([faker.internet.email(), faker.date.past()]);
+// }
 
-var q = 'INSERT INTO users (email, created_at) VALUES ?';
+// var q = 'INSERT INTO users (email, created_at) VALUES ?';
 
-connection.query(q, [data], function(error, results) {
-  if (error) throw err;
-  console.log(results);
+// connection.query(q, [data], function(error, results) {
+//   if (error) throw err;
+//   console.log(results);
+// });
+
+// connection.end();
+
+app.listen(8080, function() {
+  console.log(`Serving Join Us app on port 8080`);
 });
-
-connection.end();
